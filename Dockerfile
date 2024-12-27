7753f4337074514162e8d4b7a701746edb676d74
David Simmons <dr.desimmons@gmail.com>
2024-12-27 21:14:51 +0000

    Support pybind builds

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,15 @@
-FROM ubuntu:latest
+FROM python:3.10
 
 # Set the working directory
 WORKDIR /code
 
 RUN apt-get update && apt-get install -y --no-install-recommends \
-    build-essential libssl-dev libboost-all-dev cmake libnode-dev libhowardhinnant-date-dev \
+    build-essential libssl-dev libboost-all-dev cmake libnode-dev libhowardhinnant-date-dev pybind11-dev curl \
+    && curl -sS https://bootstrap.pypa.io/get-pip.py | python3 \
     && rm -rf /var/lib/apt/lists/*
+
+
+RUN pip install --no-cache-dir pybind11
 
 COPY . /code/
 
---
+++ src/pybind11_wrapper.cpp
@@ -0,0 +1,40 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <iostream>
+#include <optional>
+#include "PayChan.hpp"
+
+namespace py = pybind11;
+
+std::string serializePayChanAuthorization(const std::string& issuer, 
+                                           const std::string& channelId, 
+                                           const std::string& currencyCode, 
+                                           double amount) {
+    try {
+        ripple::Serializer msg;
+        ripple::uint256 channelKey = ripple::uint256(channelId.c_str());
+        ripple::IOUAmount amt(amount, 0);
+
+        ripple::Currency cur;
+        to_currency(cur, currencyCode);
+
+        std::optional<ripple::AccountID> issuerAccount = ripple::parseBase58<ripple::AccountID>(issuer);
+        if (!issuerAccount) {
+            throw std::invalid_argument("Invalid issuer address");
+        }
+
+        ripple::serializePayChanAuthorization(msg, channelKey, amt, cur, issuerAccount.value());
+
+        return strHex(msg.slice());
+
+    } catch (const std::exception& e) {
+        throw std::runtime_error(e.what());
+    }
+}
+
+PYBIND11_MODULE(paychan, m) {
+    m.doc() = "Python binding for PayChan authorization serialization"; // Optional module docstring
+    m.def("serialize_paychan_authorization", &serializePayChanAuthorization, 
+          py::arg("issuer"), py::arg("channelId"), py::arg("currencyCode"), py::arg("amount"),
+          "Serializes PayChan authorization using issuer, channelId, currencyCode, and amount");
+}
